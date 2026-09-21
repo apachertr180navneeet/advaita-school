03c4d310f24d4a9136b4dd9c79aae03c9ca77cdc
apachertr180navneeet <navneet@brenin.co>
2026-09-21 16:05:00 +0530

    feat: add responsive header and homepage styles along with navbar component

--- includes/navbar.php
+++ includes/navbar.php
@@ -45,13 +45,13 @@ $currentPage = $activePage ?? '';
     <div class="adv-header-fullwidth">
         <div class="adv-nav-ribbon">
             <!-- Far Left Corner Orange Wing -->
-            <svg class="adv-svg-corner adv-svg-corner-left" viewBox="0 0 28 64" fill="none" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="none" aria-hidden="true">
-                <path d="M0 0C18 8 22 45 0 64V0Z" fill="#F37021"/>
+            <svg class="adv-svg-corner adv-svg-corner-left" viewBox="0 0 24 42" fill="none" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="none" aria-hidden="true">
+                <path d="M0 8 C0 22 10 36 24 42 H0 V8 Z" fill="#F37021"/>
             </svg>
 
             <!-- Far Right Corner Orange Wing -->
-            <svg class="adv-svg-corner adv-svg-corner-right" viewBox="0 0 28 64" fill="none" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="none" aria-hidden="true">
-                <path d="M28 0C10 8 6 45 28 64V0Z" fill="#F37021"/>
+            <svg class="adv-svg-corner adv-svg-corner-right" viewBox="0 0 24 42" fill="none" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="none" aria-hidden="true">
+                <path d="M24 8 C24 22 14 36 0 42 H24 V8 Z" fill="#F37021"/>
             </svg>
 
             <!-- Left Desktop Menu -->
@@ -119,19 +119,21 @@ $currentPage = $activePage ?? '';
             <!-- Center Brand Crest Pod with Seamless Sculpted Shoulder Wings -->
             <div class="adv-nav-brand-pod">
                 <a href="index.php" class="adv-brand-badge-pod" title="Advaita School of Excellence">
-                    <!-- Left Shoulder Swoop Arc (Anchored to Badge Top-Left) -->
-                    <svg class="adv-shoulder-wing adv-shoulder-left" viewBox="0 0 32 16" fill="none" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="none" aria-hidden="true">
-                        <path d="M32 0C18 0 12 14 0 14V16H32V0Z" fill="#FFFFFF"/>
-                        <path d="M0 14C12 14 18 0 32 0" stroke="#F37021" stroke-width="3.5" stroke-linecap="round"/>
+                    <!-- Sculpted Crest & Shoulder Swoop SVG -->
+                    <svg class="adv-brand-crest-svg" viewBox="0 0 244 92" fill="none" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="none" aria-hidden="true">
+                        <defs>
+                            <filter id="advCrestShadow" x="-20%" y="-10%" width="140%" height="130%">
+                                <feDropShadow dx="0" dy="5" stdDeviation="5" flood-color="#021B3B" flood-opacity="0.14"/>
+                            </filter>
+                        </defs>
+                        <!-- White Sculpted Floating Badge -->
+                        <path d="M 24 12 C 24 5 30 2 38 2 H 206 C 214 2 220 5 220 12 L 208 76 C 206 84 198 88 188 88 H 56 C 46 88 38 84 36 76 Z" fill="#FFFFFF" filter="url(#advCrestShadow)"/>
+                        <!-- Left Orange Shoulder Swoop & Flank -->
+                        <path d="M 4 16 C 9 16 14 10 17 6 C 20 3 23 5 24 9 C 25 18 28 48 34 72" stroke="#F37021" stroke-width="3.5" stroke-linecap="round"/>
+                        <!-- Right Orange Shoulder Swoop & Flank -->
+                        <path d="M 240 16 C 235 16 230 10 227 6 C 224 3 221 5 220 9 C 219 18 216 48 210 72" stroke="#F37021" stroke-width="3.5" stroke-linecap="round"/>
                     </svg>
-
                     <img src="assets/images/logo-cropped.png" alt="Advaita School of Excellence" class="adv-brand-logo-pod-img">
-
-                    <!-- Right Shoulder Swoop Arc (Anchored to Badge Top-Right) -->
-                    <svg class="adv-shoulder-wing adv-shoulder-right" viewBox="0 0 32 16" fill="none" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="none" aria-hidden="true">
-                        <path d="M0 0C14 0 20 14 32 14V16H0V0Z" fill="#FFFFFF"/>
-                        <path d="M0 0C14 0 20 14 32 14" stroke="#F37021" stroke-width="3.5" stroke-linecap="round"/>
-                    </svg>
                 </a>
             </div>
 
